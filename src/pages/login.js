5094640d35bec366a3faf22a521c3a9eb25ca927
Carlos Lazo <clazo@berkeley.edu>
2018-12-02 01:35:13 -0800

    wip

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,35 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Page from "../components/page.js";
 
 class Login extends Component {
   render() {
-    return <div>hi2</div>;
+    return (
+      <Page>
+        <div className="main-login-form">
+          <p>
+            BELONG is a platform that provides a digital space of belonging for
+            youth experiencing homelessness
+          </p>
+          <div className="form-container">
+          <form className="login-form">
+              <input />
+              <input />
+            <button>Login</button>
+          </form>
+          </div>
+          <div>
+            <p>
+              If you are a youth experiencing homelessness, please reach out to
+              a San Diego Service Provider here for them to generate a login in
+              for you.
+            </p>
+          </div>
+        </div>
+
+
+      </Page>
+    );
   }
 }
 
