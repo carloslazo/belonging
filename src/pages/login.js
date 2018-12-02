294a0a54eeb06ee531f548e13c316452209d8b9a
Carlos Lazo <clazo@berkeley.edu>
2018-12-02 01:47:47 -0800

    added non function

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Page from "../components/page.js";
-import Hands from "../resources/hands.jpg"
+import Hands from "../resources/hands.jpg";
 class Login extends Component {
+   handleClick = (event) =>{
+    event.preventDefault();
+    window.location = "/home"
+  }
   render() {
     return (
       <Page background="white">
         <div className="main-login-form">
-          <img
-             className="picture-login"
-             src={Hands}
-             width="200"
-             />
+          <img className="picture-login" src={Hands} width="200" />
           <p>
             BELONG is a platform that provides a digital space of belonging for
             youth experiencing homelessness
           </p>
 
           <div className="form-container">
-          <form className="login-form">
+            <form className="login-form" onClick={this.handleClick}>
               <input />
               <input />
-            <button>Login</button>
-          </form>
+              <button>Login</button>
+            </form>
           </div>
           <div>
             <p>
@@ -32,8 +32,6 @@ class Login extends Component {
             </p>
           </div>
         </div>
-
-
       </Page>
     );
   }
