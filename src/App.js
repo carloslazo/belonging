bc253f55cc9327318944d16c6703fb5974e1f1a6
Carlos Lazo <clazo@berkeley.edu>
2018-12-02 11:27:08 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Login from "./pages/login.js";
 import Home from "./pages/home.js";
 import Chat from "./pages/chat-page.js"
+import Help from "./pages/help.js"
 
 class App extends Component {
   render() {
@@ -13,6 +14,8 @@ class App extends Component {
           <Route exact path="/" component={Login} />
           <Route path="/home" component={Home} />
           <Route path="/chat" component={Chat} />
+          <Route path="/help" component={Help} />
+
         </div>
       </Router>
     );
--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -4,10 +4,15 @@ import Error from "../resources/noun_Error_89607_000000.svg";
 class Navbar extends Component {
   handleClick = event => {
     event.preventDefault();
-    console.log(event.target.getAttribute('name'));
-    if (event.target.getAttribute('name') == "chat"){
+    console.log(event.target.getAttribute("name"));
+    if (event.target.getAttribute("name") == "chat") {
       window.location = "/chat";
-    } else {
+    } else if (event.target.getAttribute("name") == "help") {
+      window.location = "/help";
+
+  } else if (event.target.getAttribute("name") == "home") {
+    window.location = "/home";
+  } else {
       window.location = "/";
     }
   };
@@ -19,17 +24,20 @@ class Navbar extends Component {
           onClick={this.handleClick}
           type="button"
           className="fa fa-info-circle fa-6"
+          name="help"
           aria-hidden="true"
         />
         <i onClick={this.handleClick} class="fa fa-bed" aria-hidden="true" />
         <i
           onClick={this.handleClick}
-          name = "chat"
+          name="chat"
           class="fa fa-comment"
           aria-hidden="true"
         />
+      <i onClick={this.handleClick} name="home" class="fa fa-home" aria-hidden="true" />
 
         <i onClick={this.handleClick} class="fas fa-utensils" />
+          <i onClick={this.handleClick} class="fa fa-car" aria-hidden="true" />
 
         <i onClick={this.handleClick} class="fa fa-car" aria-hidden="true" />
       </div>
