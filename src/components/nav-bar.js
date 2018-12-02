d7a0417b2431080216a9aba64d7a997b10fddd66
Carlos Lazo <clazo@berkeley.edu>
2018-12-02 13:20:05 -0800

    wip

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -4,15 +4,13 @@ import Error from "../resources/noun_Error_89607_000000.svg";
 class Navbar extends Component {
   handleClick = event => {
     event.preventDefault();
-    console.log(event.target.getAttribute("name"));
     if (event.target.getAttribute("name") == "chat") {
       window.location = "/chat";
     } else if (event.target.getAttribute("name") == "help") {
       window.location = "/help";
-
-  } else if (event.target.getAttribute("name") == "home") {
-    window.location = "/home";
-  } else {
+    } else if (event.target.getAttribute("name") == "home") {
+      window.location = "/home";
+    } else {
       window.location = "/";
     }
   };
@@ -22,24 +20,29 @@ class Navbar extends Component {
       <div className="nav-bar-container">
         <i
           onClick={this.handleClick}
+          name="home"
+          class="fa fa-home"
+          aria-hidden="true"
+        />
+
+        <i
+          onClick={this.handleClick}
           type="button"
           className="fa fa-info-circle fa-6"
           name="help"
           aria-hidden="true"
         />
         <i onClick={this.handleClick} class="fa fa-bed" aria-hidden="true" />
+
+        <i onClick={this.handleClick} class="fas fa-utensils" />
+
+        <i onClick={this.handleClick} class="fa fa-car" aria-hidden="true" />
         <i
           onClick={this.handleClick}
           name="chat"
           class="fa fa-comment"
           aria-hidden="true"
         />
-      <i onClick={this.handleClick} name="home" class="fa fa-home" aria-hidden="true" />
-
-        <i onClick={this.handleClick} class="fas fa-utensils" />
-          <i onClick={this.handleClick} class="fa fa-car" aria-hidden="true" />
-
-        <i onClick={this.handleClick} class="fa fa-car" aria-hidden="true" />
       </div>
     );
   }
--- src/pages/help.js
+++ src/pages/help.js
@@ -11,7 +11,149 @@ class Help extends Component {
   render() {
     return (
       <Page background="white">
-      hey
+        <div className="help-main-container">
+          <div className="help-small-container">
+            <p
+              style={{
+                textAlign: "center",
+                fontWeight: "bold",
+                fontSize: "20px"
+              }}
+            >
+              Need Help NOW
+            </p>
+            <div className="help-rectangle">
+              <div className="icon-container">
+                <i
+                  class="fa fa-phone fa-flip-horizontal"
+                  style={{
+                    textAlign: "center",
+                    paddingRight: "10px",
+                    paddingLeft: "10px"
+                  }}
+                  aria-hidden="true"
+                />
+                <p>
+                  {" "}
+                  <b>Call</b>{" "}
+                </p>
+              </div>
+            </div>
+            <div className="help-rectangle">
+              <div className="icon-container">
+                <i
+                  class="fa fa-comment"
+                  style={{
+                    textAlign: "center",
+                    paddingRight: "10px",
+                    paddingLeft: "10px"
+                  }}
+                  aria-hidden="true"
+                />
+                <p>
+                  {" "}
+                  <b>Call</b>{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="help-small-container-1">
+            <p
+              style={{
+                textAlign: "center",
+                fontWeight: "bold",
+                fontSize: "20px"
+              }}
+            >
+              Resources
+            </p>
+            <div className="help-rectangle">
+              <div className="icon-container">
+                <i
+                  class="fa fa-mobile"
+                  style={{
+                    textAlign: "center",
+                    paddingRight: "10px",
+                    paddingLeft: "10px"
+                  }}
+                  aria-hidden="true"
+                />
+                <p>
+                  {" "}
+                  <b>Call</b>{" "}
+                </p>
+              </div>
+            </div>
+            <div className="help-rectangle">
+              <div className="icon-container">
+                <i
+                  class="fa fa-shower"
+                  style={{
+                    textAlign: "center",
+                    paddingRight: "10px",
+                    paddingLeft: "10px"
+                  }}
+                  aria-hidden="true"
+                />
+                <p>
+                  {" "}
+                  <b>Call</b>{" "}
+                </p>
+              </div>
+            </div>
+            <div className="help-rectangle">
+              <div className="icon-container">
+                <i
+                  class="fa fa-users"
+                  style={{
+                    textAlign: "center",
+                    paddingRight: "10px",
+                    paddingLeft: "10px"
+                  }}
+                  aria-hidden="true"
+                />
+                <p>
+                  {" "}
+                  <b>Call</b>{" "}
+                </p>
+              </div>
+            </div>
+            <div className="help-rectangle">
+              <div className="icon-container">
+                <i
+                  class="fa fa-circle"
+                  style={{
+                    textAlign: "center",
+                    paddingRight: "10px",
+                    paddingLeft: "10px"
+                  }}
+                  aria-hidden="true"
+                />
+                <p>
+                  {" "}
+                  <b>Call</b>{" "}
+                </p>
+              </div>
+            </div>
+            <div className="help-rectangle">
+              <div className="icon-container">
+                <i
+                  class="fa fa-bars"
+                  style={{
+                    textAlign: "center",
+                    paddingRight: "10px",
+                    paddingLeft: "10px"
+                  }}
+                  aria-hidden="true"
+                />
+                <p>
+                  {" "}
+                  <b>Call</b>{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </Page>
     );
   }
